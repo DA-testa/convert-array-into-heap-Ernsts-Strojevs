73e08b27b02f02a06bba0752737a0db1842be1bc
Ernsts-Strojevs <125212677+Ernsts-Strojevs@users.noreply.github.com>
2023-03-19 22:01:08 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,43 +1,62 @@
 # python3
-# 201RMC092 Ernsts Strojevs 16. grupa
-#
+# 201RMC092 Ernsts Strojevs 16.grupa
 
-def build_heap(data):
+import os
+from typing import List, Tuple
+
+
+def build_heap(data: List[int]) -> List[Tuple[int, int]]:
     swaps = []
     n = len(data)
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-    
 
+    for i in range(n//2-1, -1, -1):
+        j = i
+        while True:
+            left = 2*j+1
+            right = 2*j+2
+            largest = j
+            
+            if left < n and data[left] > data[largest]:
+                largest = left
+            if right < n and data[right] > data[largest]:
+                largest = right
+                
+            if largest != j:
+                data[j], data[largest] = data[largest], data[j]
+                swaps.append((j, largest))
+                j = largest
+            else:
+                break
+                
     return swaps
 
 
-def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
+def read_data_from_input() -> Tuple[str, List[int]]:
+    mode = input().strip()
+    if mode == "I":
+        n = int(input())
+        data = list(map(int, input().split()))
+    elif mode == "F":
+        file_name = input().strip()
+        file_path = os.path.join("./tests", file_name)
+        with open(file_path, "r") as f:
+            n = int(f.readline().strip())
+            data = list(map(int, f.readline().strip().split()))
+    else:
+        raise ValueError("Invalid mode")
+    return mode, data
 
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
+def print_swaps(swaps: List[Tuple[int, int]]) -> None:
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
+
+
+def main() -> None:
+    mode, data = read_data_from_input()
+    swaps = build_heap(data)
+    print_swaps(swaps)
 
 
 if __name__ == "__main__":
