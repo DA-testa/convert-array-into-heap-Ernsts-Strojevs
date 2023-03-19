1adc84cab98d100692cc519191620f56be195f20
E.Strojevs_201RMC092 <125212677+Ernsts-Strojevs@users.noreply.github.com>
2023-03-19 23:48:01 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,63 +1,53 @@
 # python3
 # 201RMC092 Ernsts Strojevs 16.grupa
 
-import os
-from typing import List, Tuple
-
-
-def build_heap(data: List[int]) -> List[Tuple[int, int]]:
-    swaps = []
+def build_heap(data):
     n = len(data)
+    swaps = []
 
-    for i in range(n//2-1, -1, -1):
-        j = i
-        while True:
-            left = 2*j+1
-            right = 2*j+2
-            largest = j
-            
-            if left < n and data[left] > data[largest]:
-                largest = left
-            if right < n and data[right] > data[largest]:
-                largest = right
-                
-            if largest != j:
-                data[j], data[largest] = data[largest], data[j]
-                swaps.append((j, largest))
-                j = largest
-            else:
-                break
-                
+    for i in range(n // 2, -1, -1):
+        min_heapify(data, i, n, swaps)
+
     return swaps
 
+def min_heapify(data, i, n, swaps):
+    left_child = 2 * i + 1
+    right_child = 2 * i + 2
+    min_index = i
 
-def read_data_from_input() -> Tuple[str, List[int]]:
-    mode = input().strip()
-    if mode == "I":
+    if left_child < n and data[left_child] < data[min_index]:
+        min_index = left_child
+
+    if right_child < n and data[right_child] < data[min_index]:
+        min_index = right_child
+
+    if i != min_index:
+        data[i], data[min_index] = data[min_index], data[i]
+        swaps.append((i, min_index))
+        min_heapify(data, min_index, n, swaps)
+
+def main():
+    input_mode = input()
+
+    if "I" in input_mode or "i" in input_mode:
         n = int(input())
         data = list(map(int, input().split()))
-    elif mode == "F":
-        file_name = input().strip()
-        file_path = os.path.join("./tests", file_name)
-        with open(file_path, "r") as f:
-            n = int(f.readline().strip())
-            data = list(map(int, f.readline().strip().split()))
-    else:
-        raise ValueError("Invalid mode")
-    return mode, data
+    elif "F" in input_mode or "f" in input_mode:
+        file_path = input()
 
+        if "a" not in file_path:
+            with open("tests/" + file_path, 'r') as f:
+                n = int(f.readline())
+                data = list(map(int, f.readline().split()))
 
-def print_swaps(swaps: List[Tuple[int, int]]) -> None:
+    assert len(data) == n
+
+    swaps = build_heap(data)
+
     print(len(swaps))
+
     for i, j in swaps:
         print(i, j)
-
-
-def main() -> None:
-    mode, data = read_data_from_input()
-    swaps = build_heap(data)
-    print_swaps(swaps)
-
 
 if __name__ == "__main__":
     main()
