979d7be407481051bbe65f401a1fadd9bd647616
E.Strojevs_201RMC092 <125212677+Ernsts-Strojevs@users.noreply.github.com>
2023-03-19 23:14:39 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,61 +1,54 @@
 # python3
 # 201RMC092 Ernsts Strojevs 16.grupa
 
-def heap_sort(arr):
-    def heapify(arr, n, i, swaps):
-        largest = i  
-        l = 2 * i + 1     
-        r = 2 * i + 2     
-     
-        if l < n and arr[i] < arr[l]:
-            largest = l
-
-        if r < n and arr[largest] < arr[r]:
-            largest = r
-
-        if largest != i:
-            arr[i], arr[largest] = arr[largest], arr[i]
-            swaps.append((i, largest))
-            heapify(arr, n, largest, swaps)
-
+def heap_sort(data):
+    n = len(data)
     swaps = []
-    n = len(arr)
-
-    for i in range(n, -1, -1):
-        heapify(arr, n, i, swaps)
-
-    for i in range(n-1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]
+    
+    # heapify the array
+    for i in range(n // 2, -1, -1):
+        sift_down(data, i, n, swaps)
+    
+    # sort the array
+    for i in range(n - 1, 0, -1):
+        data[0], data[i] = data[i], data[0]
         swaps.append((0, i))
-        heapify(arr, i, 0, swaps)
-
+        sift_down(data, 0, i, swaps)
+    
     return swaps
 
+def sift_down(data, i, n, swaps):
+    min_index = i
+    left_child = 2 * i + 1
+    if left_child < n and data[left_child] > data[min_index]:
+        min_index = left_child
+    right_child = 2 * i + 2
+    if right_child < n and data[right_child] > data[min_index]:
+        min_index = right_child
+    if i != min_index:
+        data[i], data[min_index] = data[min_index], data[i]
+        swaps.append((i, min_index))
+        sift_down(data, min_index, n, swaps)
+
 def main():
-    # get input from user or file
-    input_type = input()
-    if input_type == 'I':
-        n = int(input())
-        data = list(map(int, input().split()))
-    elif input_type == 'F':
-        file_path = input()
-        with open(file_path, 'r') as f:
+    file_or_no = input("Enter I for keyboard input or F for file input: ")
+    if file_or_no.lower() == 'i':
+        n = int(input("Enter the length of the array: "))
+        data = list(map(int, input("Enter space-separated integers: ").split()))
+    elif file_or_no.lower() == 'f':
+        file_name = input("Enter the name of the file: ")
+        with open(file_name, 'r') as f:
             n = int(f.readline())
             data = list(map(int, f.readline().split()))
-    else:
-        print("Invalid input type!")
-        return
-
-    # checks if length of data is the same as the said length
+    
+    # check if data has the expected length
     assert len(data) == n
-
-    # calls function to assess the data and give back all swaps
+    
+    # sort the array and get swaps
     swaps = heap_sort(data)
-
-    # output how many swaps were made
+    
+    # output the number of swaps and each swap
     print(len(swaps))
-
-    # output all swaps
     for i, j in swaps:
         print(i, j)
 
