daaeb82ebb3d2cd69d2f93b797c82ee17a20ef8e
E.Strojevs_201RMC092 <125212677+Ernsts-Strojevs@users.noreply.github.com>
2023-03-19 23:20:19 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,62 +1,57 @@
 # python3
 # 201RMC092 Ernsts Strojevs 16.grupa
 
-import os
-from typing import List, Tuple
+def heap_sort(data):
+    def sift_down(i, n, swaps):
+        left_child = 2 * i + 1
+        right_child = 2 * i + 2
+        min_index = i
 
+        if left_child < n and data[left_child] < data[min_index]:
+            min_index = left_child
+        if right_child < n and data[right_child] < data[min_index]:
+            min_index = right_child
+        
+        if i != min_index:
+            data[i], data[min_index] = data[min_index], data[i]
+            swaps.append((i, min_index))
+            sift_down(min_index, n, swaps)
 
-def build_heap(data: List[int]) -> List[Tuple[int, int]]:
     swaps = []
     n = len(data)
 
-    for i in range(n//2-1, -1, -1):
-        j = i
-        while True:
-            left = 2*j+1
-            right = 2*j+2
-            largest = j
-            
-            if left < n and data[left] > data[largest]:
-                largest = left
-            if right < n and data[right] > data[largest]:
-                largest = right
-                
-            if largest != j:
-                data[j], data[largest] = data[largest], data[j]
-                swaps.append((j, largest))
-                j = largest
-            else:
-                break
-                
+    # build heap
+    for i in range(n // 2 - 1, -1, -1):
+        sift_down(i, n, swaps)
+
+    # perform heap sort
+    for i in range(n - 1, 0, -1):
+        data[0], data[i] = data[i], data[0]
+        swaps.append((0, i))
+        sift_down(0, i, swaps)
+
     return swaps
 
 
-def read_data_from_input() -> Tuple[str, List[int]]:
-    mode = input().strip()
-    if mode == "I":
+def main():
+    # read input
+    input_type = input()
+    if input_type.lower() == 'i':
         n = int(input())
         data = list(map(int, input().split()))
-    elif mode == "F":
-        file_name = input().strip()
-        file_path = os.path.join("./tests", file_name)
-        with open(file_path, "r") as f:
-            n = int(f.readline().strip())
-            data = list(map(int, f.readline().strip().split()))
-    else:
-        raise ValueError("Invalid mode")
-    return mode, data
+    elif input_type.lower() == 'f':
+        file_path = input()
+        with open(f"tests/{file_path}", "r") as f:
+            n = int(f.readline())
+            data = list(map(int, f.readline().split()))
+    
+    # perform heap sort
+    swaps = heap_sort(data)
 
-
-def print_swaps(swaps: List[Tuple[int, int]]) -> None:
+    # output result
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
-
-def main() -> None:
-    mode, data = read_data_from_input()
-    swaps = build_heap(data)
-    print_swaps(swaps)
 
 
 if __name__ == "__main__":
